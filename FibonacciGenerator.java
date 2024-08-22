d79718a6abe29535f5b68662817731cf7284c7f1
Neha0108 <112578569+Neha0108@users.noreply.github.com>
2024-08-22 11:05:40 +0530

    Update FibonacciGenerator.java

--- FibonacciGenerator.java
+++ FibonacciGenerator.java
@@ -1,18 +1,11 @@
-import java.util.Scanner;
-
 public class FibonacciGenerator {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter the number of terms: ");
-        int terms = scanner.nextInt();
+        int terms = 10;  // Predefined number of terms
 
         System.out.println("Fibonacci sequence up to " + terms + " terms:");
         for (int i = 0; i < terms; i++) {
             System.out.print(fibonacci(i) + " ");
         }
-
-        scanner.close();
     }
 
     public static int fibonacci(int n) {
